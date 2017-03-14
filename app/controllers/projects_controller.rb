5ad062ebd62ec6661cf281157e9197aad8ce418f
dmartinez <dmartinez@localhost.localdomain>
2017-03-14 11:28:07 -0700

    Se modifico para que el usuario se tome de la session y que al generar el proyecto no lo solicite

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,7 +24,7 @@ class ProjectsController < ApplicationController
   # POST /projects
   # POST /projects.json
   def create
-    @project = Project.new(project_params)
+    @project = current_user.projects.new(project_params)
 
     respond_to do |format|
       if @project.save
